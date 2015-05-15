67047c5ecffbae83f11e02bfb57b1d46840c61ca
Serkin Alexander <serkin.alexander@gmail.com>
2015-05-15 13:49:48 +0300

    Implement CustomArrayObject

--- src/CustomArrayObject.php
+++ src/CustomArrayObject.php
@@ -2,5 +2,24 @@
 
 namespace Volan;
 
-class CustomRecursiveArrayIterator extends \RecursiveArrayIterator
-{}
+class CustomArrayObject extends \ArrayObject
+{
+    /**
+     * Gets key from current array.
+     * Excludes _type from final array.
+     *
+     * @return array
+     */
+    public function getArrayKeys()
+    {
+        $returnValue = [];
+
+        foreach (array_keys($this->getArrayCopy()) as $key):
+            if ($key != '_type'):
+                $returnValue[] = $key;
+        endif;
+        endforeach;
+
+        return $returnValue;
+    }
+}
