fc71ce5c00fda8e66bc2c8337a209c3801aaa732
Serkin Alexander <serkin.alexander@gmail.com>
2015-05-17 14:13:29 +0300

    Tests for number and boolean validators

---
+++ tests/unit/Validator/Number.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class Volan_Validator_Numver extends PHPUnit_Framework_TestCase
+{
+
+    public function testErrorOnValidation()
+    {
+        require dirname(__DIR__).'/fixture/fixture18.php';
+
+        $validator = new \Volan\Volan($schema);
+        $result = $validator->validate($arr);
+
+        $this->assertFalse($result);
+
+        $expectedErrorCode = $validator->getErrorInfo()['code'];
+
+        $this->assertEquals(\Volan\Volan::ERROR_NODE_IS_NOT_VALID, $expectedErrorCode);
+    }
+    
+    public function testSuccessValidation()
+    {
+        require dirname(__DIR__).'/fixture/fixture19.php';
+
+        $validator = new \Volan\Volan($schema);
+        $result = $validator->validate($arr);
+
+        $this->assertTrue($result);
+    }
+}
