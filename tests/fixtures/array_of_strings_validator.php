a7ed7778bbe799af49f9ead971eb84bd58499c3f
Serkin Alexander <serkin.alexander@gmail.com>
2015-06-22 11:58:44 +0300

    Add setErrorDescription to fixture class

--- tests/fixtures/array_of_strings_validator.php
+++ tests/fixtures/array_of_strings_validator.php
@@ -12,12 +12,23 @@ class array_of_strings_validator extends AbstractValidator
     public function isValid($nodeData)
     {
 
-        foreach($nodeData as $value) {
-            if (!is_string($value)) {
-                return false;
+        $returnValue = true;
+
+
+        if(!is_array($nodeData)) {
+
+            $this->setErrorDescription('Value supposed to be an array');
+            $returnValue = false;
+
+        } else {
+
+            foreach ($nodeData as $value) {
+                if (!is_string($value)) {
+                    $returnValue = false;
+                }
             }
         }
 
-        return true;
+        return $returnValue;
     }
 }
