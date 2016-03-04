12dd49282adcb311285e82b526aed0ff80257430
Alexander Serkin <serkin.alexander@gmail.com>
2016-03-04 15:49:02 +0300

    Refactor tests

--- tests/unit/Volan.php
+++ tests/unit/Volan.php
@@ -1,6 +1,7 @@
 <?php
 
 use Volan\Volan as v;
+use Volan\ValidatorResult;
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
@@ -16,259 +17,312 @@ class Volan extends PHPUnit_Framework_TestCase
         parent::setUp();
     }
 
+    public function getSchemaFixture() {
+
+
+        return [
+            'root' => [
+                'title' => [
+                    '_type' => 'required_string'
+                ],
+                'characters' => [
+                    '_type' => 'array_of_strings'
+                ],
+                'price' => [
+                    '_type' => 'number'
+                ],
+                'tags' => [
+                    '_type' => 'array'
+                ],
+                'instock' => [
+                    '_type' => 'required_boolean'
+                ],
+                'reserved' => [
+                    '_type' => 'boolean'
+                ],
+                'author' => [
+                    '_type' => 'required_array',
+                    'name' => [
+                        '_type' => 'required_string'
+                    ],
+                ],
+                'comments' => [
+                    '_type' => 'nested_array',
+                    'comment' => [
+                        '_type' => 'required_string'
+                    ],
+                    'userid' => [
+                        '_type' => 'required_number'
+                    ],
+                    'rating' => [
+                        '_type' => 'number'
+                    ]
+                ]
+            ]
+        ];
+
+    }
+
+
+    public function getDataFixture() {
+
+        return  [
+            'title'     => 'The Idiot',
+            'instock'   => true,
+            'reserved'  => false,
+            'price'     => 60,
+            'characters' => ['Lev Nikolayevich Myshkin'],
+            'author' => ['name' => 'Fyodor Dostoyevsky'],
+            'tags'      => ['novel', 'russia'],
+            'comments'  => [
+                [
+                    'comment'   => 'Good book',
+                    'userid'    => 1,
+                    'rating'    => 10
+                ],
+                [
+                    'comment'   => 'I love it',
+                    'userid'    => 2,
+                    'rating'    => 10
+                ],
+            ]
+        ];
+    }
+
     public function testSuccessfulValidation()
     {
-        require $this->fixtureFileName;
 
-        $validator = new v($schema);
-        $result = $validator->validate($arr);
+        $validator = new v($this->getSchemaFixture());
+        $result = $validator->validate($this->getDataFixture());
 
-        $this->assertTrue($result);
+        $this->assertTrue($result->isValid());
     }
 
     public function testErrorOnMissingRootNode()
     {
-        require $this->fixtureFileName;
 
         $schema = ['roooot' => []];
 
-        $validator = new v($schema);
-        $result = $validator->validate($arr);
+        $result = (new v($schema))->validate($this->getDataFixture());
 
-        $this->assertFalse($result);
-        $this->assertEquals(v::ERROR_SCHEMA_HAS_NO_ROOT_ELEMENT, $validator->getErrorInfo()['code']);
-        $this->assertEquals('root', $validator->getErrorInfo()['node']);
+        $this->assertFalse($result->isValid());
+        $this->assertEquals(ValidatorResult::ERROR_SCHEMA_HAS_NO_ROOT_ELEMENT, $result->getErrorCode());
+        $this->assertEquals('root', $result->getErrorNode());
 
     }
 
     public function testErrorOnMissingValidatorClaa()
     {
 
-        require $this->fixtureFileName;
-
+        $schema = $this->getSchemaFixture();
         $schema['root']['price'] = ['_type' => 'numbers'];  // Let's corrupt data. There is no validator NUMBERS
 
-        $validator = new v($schema);
-        $result = $validator->validate($arr);
+        $result = (new v($schema))->validate($this->getDataFixture());
 
-        $this->assertFalse($result);
-        $this->assertEquals(v::ERROR_VALIDATOR_CLASS_NOT_FOUND, $validator->getErrorInfo()['code']);
-        $this->assertEquals('root.price', $validator->getErrorInfo()['node']);
+        $this->assertFalse($result->isValid());
+        $this->assertEquals(ValidatorResult::ERROR_VALIDATOR_CLASS_NOT_FOUND, $result->getErrorCode());
+        $this->assertEquals('root.price', $result->getErrorNode());
 
     }
 
     public function testErrorOnMissingTypeField()
     {
-        require $this->fixtureFileName;
 
+        $schema = $this->getSchemaFixture();
         unset($schema['root']['price']['_type']);
 
-        $validator = new v($schema);
-        $result = $validator->validate($arr);
+        $result = (new v($schema))->validate($this->getDataFixture());
 
-        $this->assertFalse($result);
-        $this->assertEquals(v::ERROR_NODE_HAS_NO_FIELD_TYPE, $validator->getErrorInfo()['code']);
-        $this->assertEquals('root.price', $validator->getErrorInfo()['node']);
+        $this->assertFalse($result->isValid());
+        $this->assertEquals(ValidatorResult::ERROR_NODE_HAS_NO_FIELD_TYPE, $result->getErrorCode());
+        $this->assertEquals('root.price', $result->getErrorNode());
     }
 
     public function testErrorOnMissingTypeFieldInDepth()
     {
-        require $this->fixtureFileName;
 
+        $schema = $this->getSchemaFixture();
         unset($schema['root']['comments']['comment']['_type']);
 
-        $validator = new v($schema);
-        $result = $validator->validate($arr);
 
-        $this->assertFalse($result);
-        $this->assertEquals(v::ERROR_NODE_HAS_NO_FIELD_TYPE, $validator->getErrorInfo()['code']);
-        $this->assertEquals('comments.comment', $validator->getErrorInfo()['node']);
+        $result = (new v($schema))->validate($this->getDataFixture());
+
+        $this->assertFalse($result->isValid());
+        $this->assertEquals(ValidatorResult::ERROR_NODE_HAS_NO_FIELD_TYPE, $result->getErrorCode());
+        $this->assertEquals('comments.comment', $result->getErrorNode());
     }
 
 
 
     public function testErrorOnInvalidNode()
     {
-        require $this->fixtureFileName;
 
+        $arr = $this->getDataFixture();
         $arr['instock'] = 'instock'; // Let's corrupt data. Instock has to be BOOLEAN
 
-        $validator = new v($schema);
-        $result = $validator->validate($arr);
+        $result = (new v($this->getSchemaFixture()))->validate($arr);
 
-        $this->assertFalse($result);
-        $this->assertEquals(v::ERROR_NODE_IS_NOT_VALID, $validator->getErrorInfo()['code']);
-        $this->assertEquals('root.instock', $validator->getErrorInfo()['node']);
+        $this->assertFalse($result->isValid());
+        $this->assertEquals(ValidatorResult::ERROR_NODE_IS_NOT_VALID, $result->getErrorCode());
+        $this->assertEquals('root.instock', $result->getErrorNode());
     }
 
     public function testErrorOnInvalidNodeInDepth()
     {
-        require $this->fixtureFileName;
 
+        $arr = $this->getDataFixture();
         $arr['comments'][0]['userid'] = 'John'; // Let's corrupt data. Userid has to be NUMBER
 
-        $validator = new v($schema);
-        $result = $validator->validate($arr);
+        $result = (new v($this->getSchemaFixture()))->validate($arr);
 
-        $this->assertFalse($result);
-        $this->assertEquals(v::ERROR_NODE_IS_NOT_VALID, $validator->getErrorInfo()['code']);
-        $this->assertEquals('comments.userid', $validator->getErrorInfo()['node']);
+        $this->assertFalse($result->isValid());
+        $this->assertEquals(ValidatorResult::ERROR_NODE_IS_NOT_VALID, $result->getErrorCode());
+        $this->assertEquals('comments.userid', $result->getErrorNode());
     }
 
     public function testErrorInNestedNode()
     {
-        require $this->fixtureFileName;
 
+        $arr = $this->getDataFixture();
         $arr['comments'] = ['comment' => 'comment']; // Let's corrupt data. Comments has to be nested
 
-        $validator = new v($schema);
-        $result = $validator->validate($arr);
+        $result = (new v($this->getSchemaFixture()))->validate($arr);
 
-        $this->assertFalse($result);
-        $this->assertEquals(v::ERROR_NESTED_ELEMENT_NOT_VALID, $validator->getErrorInfo()['code']);
-        $this->assertEquals('root.comments', $validator->getErrorInfo()['node']);
+        $this->assertFalse($result->isValid());
+        $this->assertEquals(ValidatorResult::ERROR_NESTED_ELEMENT_NOT_VALID, $result->getErrorCode());
+        $this->assertEquals('root.comments', $result->getErrorNode());
     }
 
     public function testErrorOnMissingRequiredField()
     {
-        require $this->fixtureFileName;
 
+        $arr = $this->getDataFixture();
         unset($arr['title']); // Let's corrupt data. Title is REQUIRED
 
-        $validator = new v($schema);
-        $result = $validator->validate($arr);
+        $result = (new v($this->getSchemaFixture()))->validate($arr);
 
-        $this->assertFalse($result);
-        $this->assertEquals(v::ERROR_REQUIRED_FIELD_IS_EMPTY, $validator->getErrorInfo()['code']);
-        $this->assertEquals('root.title', $validator->getErrorInfo()['node']);
+        $this->assertFalse($result->isValid());
+        $this->assertEquals(ValidatorResult::ERROR_REQUIRED_FIELD_IS_EMPTY, $result->getErrorCode());
+        $this->assertEquals('root.title', $result->getErrorNode());
     }
 
     public function testErrorOnMissingRequiredFieldInDepth()
     {
-        require $this->fixtureFileName;
 
+        $arr = $this->getDataFixture();
         unset($arr['comments'][0]['comment']); // Let's corrupt data. Title is REQUIRED
 
-        $validator = new v($schema);
-        $result = $validator->validate($arr);
+        $result = (new v($this->getSchemaFixture()))->validate($arr);
 
-        $this->assertFalse($result);
-        $this->assertEquals(v::ERROR_REQUIRED_FIELD_IS_EMPTY, $validator->getErrorInfo()['code']);
-        $this->assertEquals('comments.comment', $validator->getErrorInfo()['node']);
+        $this->assertFalse($result->isValid());
+        $this->assertEquals(ValidatorResult::ERROR_REQUIRED_FIELD_IS_EMPTY, $result->getErrorCode());
+        $this->assertEquals('comments.comment', $result->getErrorNode());
     }
 
     public function testErrorOnExtraKeys()
     {
-        require $this->fixtureFileName;
 
+        $arr = $this->getDataFixture();
         $arr['extrakey'] = []; // Let's corrupt data. Add excessive keys
 
-        $validator = new v($schema);
-        $result = $validator->validate($arr);
+        $result = (new v($this->getSchemaFixture()))->validate($arr);
 
-        $this->assertFalse($result);
-        $this->assertEquals(v::ERROR_NODE_HAS_EXCESSIVE_KEYS, $validator->getErrorInfo()['code']);
-        $this->assertEquals('root', $validator->getErrorInfo()['node']);
+        $this->assertFalse($result->isValid());
+        $this->assertEquals(ValidatorResult::ERROR_NODE_HAS_EXCESSIVE_KEYS, $result->getErrorCode());
+        $this->assertEquals('root', $result->getErrorNode());
     }
 
     public function testErrorOnExtraKeysInDepth()
     {
-        require $this->fixtureFileName;
 
+        $arr = $this->getDataFixture();
         $arr['comments'][0]['extrakey'] = []; // Let's corrupt data. Add excessive keys
 
-        $validator = new v($schema);
-        $result = $validator->validate($arr);
+        $result = (new v($this->getSchemaFixture()))->validate($arr);
 
-        $this->assertFalse($result);
-        $this->assertEquals(v::ERROR_NODE_HAS_EXCESSIVE_KEYS, $validator->getErrorInfo()['code']);
-        $this->assertEquals('root.comments', $validator->getErrorInfo()['node']);
+        $this->assertFalse($result->isValid());
+        $this->assertEquals(ValidatorResult::ERROR_NODE_HAS_EXCESSIVE_KEYS, $result->getErrorCode());
+        $this->assertEquals('root.comments', $result->getErrorNode());
     }
 
     public function testSettingRequiredMode()
     {
-        require $this->fixtureFileName;
 
+        $arr = $this->getDataFixture();
         unset($arr['author']);
 
-        $validator = new v($schema);
+        $validator = new v($this->getSchemaFixture());
         $validator->setRequiredMode(false);
 
         $result = $validator->validate($arr);
 
-        $this->assertTrue($result);
+        $this->assertTrue($result->isValid());
     }
 
     public function testSettingRequiredModeInDepth()
     {
-        require $this->fixtureFileName;
 
+        $arr = $this->getDataFixture();
         unset($arr['comments'][0]['userid']);
 
-        $validator = new v($schema);
+        $validator = new v($this->getSchemaFixture());
         $validator->setRequiredMode(false);
 
         $result = $validator->validate($arr);
 
-        $this->assertTrue($result);
+        $this->assertTrue($result->isValid());
     }
 
     public function testStrictMode()
     {
-        require $this->fixtureFileName;
 
+        $arr = $this->getDataFixture();
         $arr['extrakey'] = []; // Let's corrupt data. Add excessive keys
 
-        $validator = new v($schema, false);
+        $validator = new v($this->getSchemaFixture(), false);
         $result = $validator->validate($arr);
 
-        $this->assertTrue($result);
+        $this->assertTrue($result->isValid());
     }
 
     public function testStrictModeInDepth()
     {
-        require $this->fixtureFileName;
 
+        $arr = $this->getDataFixture();
         $arr['comments'][0]['extrakey'] = []; // Let's corrupt data. Add excessive keys
 
 
-        $validator = new v($schema, false);
+        $validator = new v($this->getSchemaFixture(), false);
         $result = $validator->validate($arr);
 
-        $this->assertTrue($result);
+        $this->assertTrue($result->isValid());
     }
 
-    public function testSettingLogger()
+    public function testLogger()
     {
 
-        require $this->fixtureFileName;
+        $arr = $this->getDataFixture();
+        $arr['comments'][0]['extrakey'] = []; // Let's corrupt data. Add excessive keys
 
-        $filename =  tempnam(sys_get_temp_dir(), 'log');
+        $validator = new v($this->getSchemaFixture());
+        $result = $validator->validate($arr);
 
-        $log = new Logger('name');
-        $log->pushHandler(new StreamHandler($filename));
+        $this->assertNotFalse(strpos($result->getLog(), 'element has excessive keys'));
 
-        $validator = new v($schema);
-        $validator->setLogger($log);
-        $validator->validate($arr);
 
-        $this->assertTrue(filesize($filename) > 0);
-
-        @unlink($filename);
     }
 
     public function testGetErrorDescriptionFromValidator()
     {
 
-        require $this->fixtureFileName;
+        $arr = $this->getDataFixture();
 
         $arr['characters'] = 'Sctring'; // Let's corrupt data. Value has to be array of string
 
-        $validator = new v($schema);
-        $result = $validator->validate($arr);
+        $result = (new v($this->getSchemaFixture()))->validate($arr);
 
-        $this->assertFalse($result);
-        $this->assertEquals(1, preg_match('/Value supposed to be an array/', $validator->getErrorInfo()['error']));
+        $this->assertFalse($result->isValid());
+        $this->assertEquals(1, preg_match('/Value supposed to be an array/', $result->getErrorMessage()));
 
     }
 
